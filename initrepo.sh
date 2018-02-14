44bc3879d5b477706001934746592f24e1cfecc2
Lilith Tundrus <echofox123@gmail.com>
2018-02-13 20:19:22 -0500

    Spelling/comment fixes. Remove ls command --not really needed

--- initrepo.sh
+++ initrepo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+# No color
+NC='\033[0m'
 
 printf "\nNode.js Repository init Script by Lilith Tundrus v1.0.0\n"
 
@@ -9,7 +10,7 @@ printf "\nCreating Directories...\n"
 mkdir -v ./logs
 mkdir -v ./lib
 
-# CHeck for Git
+# Check for Git
 printf "\nChecking for Git...\n"
 command -v git  2>&1 || { echo >&2 "I require Git but it's not installed.  Aborting."; exit 1; }
 printf "${GREEN}Git exists!${NC}\n"
@@ -22,7 +23,6 @@ printf "${GREEN}NPM exists!${NC}\n"
 # For writing to README.MD
 currentDir=${PWD##*/}
 
-
 # Create our files (README, etc)
 printf "\n\nWriting Initial files..."
 touch README.MD
@@ -30,13 +30,8 @@ touch LICENSE
 touch .gitignore
 touch index.js
 echo "node_modules" > .gitignore
+# Write PWD name to the README file by default 
 echo "# ${currentDir}" > README.MD
-
-printf "\n\n"
-
-
-# list the files we generated
-ls -alrth
 
 # Run our repo inits
 printf "\n\n"
